9aceb7a9bb945979e41d2577c2a622b32f03079f
NahumFG <ngutierrez2507@gmail.com>
2024-05-20 21:22:54 -0500

    backend: refactorización de admins

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -3,15 +3,13 @@ from django.contrib import admin
 from userauths.models import Profile, User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["email", "full_name", "phone_number"]
+    list_display = ("email", "full_name", "phone_number")
 
 
+@admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ["full_name", "gender", "country"]
-    search_fields = ["full_name", "date"]
-    list_filter = ["date"]
-
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Profile, ProfileAdmin)
+    list_display = ("full_name", "gender", "country")
+    search_fields = ("full_name", "date")
+    list_filter = ("date",)
